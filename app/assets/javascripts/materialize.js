ffa1e0adf36faaa7d0bcf27356ab84e9195073f9
Anibal <anibalmart97@gmail.com>
2017-06-12 12:03:43 -0600

    primer avance aplicaciones graficas

---
+++ app/assets/javascripts/materialize.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('select').material_select();
+  $('.parallax').parallax();
+  $(".button-collapse").sideNav();
+  $(".card-reveal").span();
+   $('.modal').modal();
+});
